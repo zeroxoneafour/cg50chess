64d1a7a590bd601a312cb5f323923d1594e4b616
Vaughan Milliman <vaughanm1@protonmail.com>
2023-05-07 21:20:18 -0500

    got sdk working

---
+++ build/main.d
@@ -0,0 +1,5 @@
+main.o: /home/vaughanm/my-git/cg50chess/src/main.c \
+ /home/vaughanm/my-git/cg50chess/src/libfxcg/include/fxcg/display.h \
+ /home/vaughanm/my-git/cg50chess/src/libfxcg/include/fxcg/keyboard.h
+/home/vaughanm/my-git/cg50chess/src/libfxcg/include/fxcg/display.h:
+/home/vaughanm/my-git/cg50chess/src/libfxcg/include/fxcg/keyboard.h:
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <fxcg/display.h>
+#include <fxcg/keyboard.h>
+int main() {
+  PrintXY(3,8,"--Hello World!", TEXT_MODE_NORMAL, TEXT_COLOR_RED); // Place it somewhere on the screen.  Note that x,y are 1-indexed.
+  int key;
+  while(1)
+    GetKey(&key); // Loop forever
+
+  return 0; // Needed if you have main returning int, but will never be reached.
+}:
